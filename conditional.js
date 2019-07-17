a8c009ac87b2e2e78b6cb6f095a2325eebe947e6
Denny Septiady <dennyseptiady2012@gmail.com>
2019-07-17 20:35:38 +0700

    Tambah conditional

---
+++ conditional.js
@@ -0,0 +1,60 @@
+// >, <, >==, <==, ==, !=, !==, ===
+
+//onsole.log('Apakah sama ? '. 1 === '1');
+//1 === '1' //false
+//1 == '1' //true
+
+//1. if
+
+if(1 > 0) {
+    console.log('[IF] satu lebih dari o');
+};
+
+//2. if - else
+if (1 != 1) {
+    console.log('[IF ELSE] 1 tidak sama dengan 1');
+} else {
+    console.log('[IF ELSE] 1 sama dengan 1');
+}
+
+//3. if - else - if
+if (1 != 1) {
+    console.log('[IF ELSE IF] 1 tidak sama dengan 1');
+} else if (1 > 1) {
+    console.log('[IF ELSE IF] tapi 1 lebih dari 0');
+}
+
+//4. if - else - if - else
+if (1 != 1) {
+    console.log('[IF ELSE IF] 1 tidak sama dengan 1');
+} else if (1 > 1) {
+    console.log('[IF ELSE IF] tapi 1 lebih dari 0');
+} else {
+    console.log('[IF ELSE IF ELSE] ambil semua cuy');
+}
+
+//5. if - if
+if (1 > 0) {
+    console.log('ternyata benar');
+        if (2 > 1) {
+        console.log('2 lebih dari 1');
+    }
+        console.log('yg 2 > 1 bisa jadi salah bro');
+}
+
+5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
